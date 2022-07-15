096da8bab0cb313ddef9d964a3098966f3f2a987
muhammaddiyor qobulov <muhammaddiyor20032808@gmail.com>
2022-07-15 10:51:51 +0500

    hex-btn-combination

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function toggleZone(hideZone, showZone, display = "block") {
   hideZone.style.display = "none";
   if (showZone) {
-    hideZone.style.display = `${display}`;
+    showZone.style.display = `${display}`;
   }
 }
 
@@ -15,18 +15,28 @@ function randomColor() {
   return res;
 }
 
-function renderBtns(value, appendZone) {
+function renderBtns(value, appendZone, text) {
   for (let i = 0; i < value; i++) {
     let btn = document.createElement("button");
-
-    btn.style.background = randomColor();
+    let color = randomColor();
+    btn.style.background = color;
     btn.style.width = "250px";
     btn.style.height = "150px";
     btn.style.display = "flex";
     btn.style.justifyContent = "center";
     btn.style.alignItems = "center";
     btn.style.fontSize = "24px";
-    
+    btn.innerHTML = color;
+    btn.classList = "btn";
+
     appendZone.appendChild(btn);
+
+    btn.addEventListener("click", () => {
+      text.innerHTML = color;
+      const body = document.querySelector("body");
+      body.style.background = color;
+    });
   }
 }
+
+module.exports = { toggleZone, renderBtns, randomColor };
